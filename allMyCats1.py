58ef2d6d6db89c2db78479f3029320c4c0b7d2b9
Dennis Nderitu <dknderitu@gmail.com>
2017-10-09 18:31:27 +0300

    how i differentiate my cats

---
+++ allMyCats1.py
@@ -0,0 +1,18 @@
+# All my cats
+
+print('Enter the name of cat 1:')
+catName1 = raw_input()
+print('Enter the name of cat 2:')
+catName2 = raw_input()
+print('Enter the name of cat 3:')
+catName3 = raw_input()
+print('Enter the name of cat 4:')
+catName4 = raw_input()
+print('Enter the name of cat 5:')
+catName5 = raw_input()
+print('Enter the name of cat 6:')
+catName6 = raw_input()
+
+print('The cat names are:')
+print(catName1 + ' ' + catName2 + ' ' + catName3 + ' ' + catName4 + ' ' +
+catName5 + ' ' + catName6)
---
+++ allMyCats2.py
@@ -0,0 +1,15 @@
+# All My Cats
+
+catNames = []
+
+while True:
+	print('Enter the name of cat ' + str(len(catNames) + 1) + '(Or enter nothing to stop.):') 
+
+	name = raw_input()
+	if name == '':
+		break
+	catNames = catNames + [name] # list concatenation
+	print('The cat names are:')
+
+	for name in catNames:
+		print('  ' + name)
